f210d4dae4987450b256382af0a487686036224d
王鹏 <383557980@qq.com>
2019-01-21 18:25:29 +0800

    'index'

--- compass/pages/index/index.js
+++ compass/pages/index/index.js
@@ -1,54 +1,122 @@
-//index.js
-//获取应用实例
-const app = getApp()
-
 Page({
-  data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
+  onShareAppMessage() {
+    return {
+      title: '监听重力感应数据',
+      path: 'page/API/pages/on-accelerometer-change/on-accelerometer-change'
     }
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+
+  data: {
+    x: 0,
+    y: 0,
+    z: 0,
+    enabled: true
+  },
+  onReady() {
+    this.drawBigBall()
+    const that = this
+
+    this.position = {
+      x: 70,
+      y: 70,
+      vx: 0,
+      vy: 0,
+      ax: 0,
+      ay: 0
+    }
+    wx.onAccelerometerChange(function (res) {
+      that.setData({
+        x: res.x.toFixed(2),
+        y: res.y.toFixed(2),
+        z: res.z.toFixed(2)
+      })
+      that.position.ax = Math.sin(res.x * Math.PI / 2)
+      that.position.ay = -Math.sin(res.y * Math.PI / 2)
+      // that.drawSmallBall()
     })
+
+    this.interval = setInterval(function () {
+      that.drawSmallBall()
+    }, 17)
+  },
+  drawBigBall() {
+    const context = wx.createContext()
+    context.beginPath(0)
+    context.arc(70, 70, 70, 0, Math.PI * 2)
+    context.setFillStyle('#ffffff')
+    context.setStrokeStyle('#aaaaaa')
+    context.fill()
+    // context.stroke()
+    wx.drawCanvas({
+      canvasId: 'big-ball',
+      actions: context.getActions()
+    })
+  },
+  drawSmallBall() {
+    const p = this.position
+    let strokeStyle = 'rgba(1,1,1,0)'
+
+    p.x += p.vx
+    p.y += p.vy
+    p.vx += p.ax
+    p.vy += p.ay
+
+    // eslint-disable-next-line
+    if (Math.sqrt(Math.pow(Math.abs(p.x) - 70, 2) + Math.pow(Math.abs(p.y) - 70, 2)) >= 115) {
+      if (p.x > 70 && p.vx > 0) {
+        p.vx = 0
+      }
+      if (p.x < 70 && p.vx < 0) {
+        p.vx = 0
+      }
+      if (p.y > 70 && p.vy > 0) {
+        p.vy = 0
+      }
+      if (p.y < 70 && p.vy < 0) {
+        p.vy = 0
+      }
+      strokeStyle = '#ff0000'
+    }
+
+    const context = wx.createContext()
+    context.beginPath(0)
+    context.arc(p.x, p.y, 15, 0, Math.PI * 2)
+    context.setFillStyle('#1aad19')
+    context.setStrokeStyle(strokeStyle)
+    context.fill()
+    // context.stroke()
+    wx.drawCanvas({
+      canvasId: 'small-ball',
+      actions: context.getActions()
+    })
+  },
+  startAccelerometer() {
+    if (this.data.enabled) {
+      return
+    }
+    const that = this
+    wx.startAccelerometer({
+      success() {
+        that.setData({
+          enabled: true
+        })
+      }
+    })
+  },
+  stopAccelerometer() {
+    if (!this.data.enabled) {
+      return
+    }
+    const that = this
+    wx.stopAccelerometer({
+      success() {
+        that.setData({
+          enabled: false
+        })
+      }
+    })
+  },
+  onUnload() {
+    clearInterval(this.interval)
   }
 })
