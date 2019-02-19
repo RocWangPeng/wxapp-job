a83e2a19c7ce83eaf17e00b0a1d38f5f3ac0fee1
王鹏 <383557980@qq.com>
2019-02-19 15:38:18 +0800

    开始开发团队关联之前的代码

--- common-card/utils/util.js
+++ common-card/utils/util.js
@@ -16,7 +16,7 @@ const formatNumber = n => {
 
 // 验证手机号
 function regPhone(val) {
-	var myreg = /^1(3|4|5|7|8)[0-9]\d{8}$/;
+  var myreg = /^0?(13[0-9]|14[5-9]|15[012356789]|166|17[0-8]|18[0-9]|19[8-9])[0-9]{8}$/;
 	if (myreg.test(val)) {
 		return true
 	} else {
